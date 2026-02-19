60fdab35b273e7298e62bed9fcade9a62bb000ca
Siri <sirianil2004@gmail.com>
2026-02-19 10:38:55 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/*/.{html,js}"   // scans all HTML and JS files in src folder
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
+
